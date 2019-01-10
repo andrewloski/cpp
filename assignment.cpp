4235e73a864a7e5553fcecd99194421e45f64c22
andrewloski <38717839+andrewloski@users.noreply.github.com>
2019-01-10 21:59:19 +0800

    update

--- assignment.cpp
+++ assignment.cpp
@@ -1,68 +1,66 @@
 #include<cstring>
 #include<cstdio>
-class cstring {
-	public:
-		cstring(char* s = nullptr);
-		cstring(const cstring& s);
-		~cstring(void);
-		cstring& operator = (const cstring& s);
-		void print();
-	private:
-		char * s;
+class cs {
+  public:
+    cs(char * s = nullptr);
+    cs(const cs& s);
+    ~cs(void);
+    cs& operator = (const cs& s);
+    void print();
+  private:
+    char* s;
 };
-cstring::cstring(char *s) {
-	if(s == nullptr){
-		this -> s = new char[1];
-		this -> s[0] = '\0';
-	}
-	else {
-		this -> s = new char[strlen(s) + 1];
-		strcpy(this -> s, s);
-	}
+cs::cs(char* s) {
+  if(s == nullptr) {
+    this -> s = new char[1];
+    this -> s[0] = '\0';
+  }
+  else {
+    this -> s = new char[strlen(s) + 1];
+    strcpy(this -> s, s);
+  }
 }
-cstring::cstring(const cstring& s){
-	this -> s = new char[strlen(s.s) + 1];
-	strcpy(this -> s, s.s);
-		
+cs::cs(const cs& s) {
+  this -> s = new char[strlen(s.s) + 1];
+  strcpy(this -> s, s.s);
 }
-cstring::~cstring(){
-	delete[] s;
+cs::~cs(){
+  delete[] s;
 }
-cstring& cstring::operator = (const cstring& s){
-	if(this == &s)
-		return *this;
-	delete[] this -> s;
-	this -> s = nullptr;
-	this -> s = new char[strlen(s.s) + 1];
-	strcpy(this -> s, s.s);
-	return *this;
+cs& cs::operator = (const cs& s) {
+    if(this != &s) {
+      cs str(s);
+      char* p = str.s;
+      str.s = this -> s;
+      this -> s = p;
+    }
+    return *this;
 }
-void cstring::print(){
-	printf("%s\n", s);
+void cs::print() {
+  printf("%s\n", s);
 }
-void assignment(){
-	cstring s1("assignment");
-	cstring s2;
-	s2 = s1;
-	s2.print();
+void assignment() {
+    cs s1("assignment");
+    cs s2;
+    s2 = s1;
+    s2.print();
 }
-void continuousAssignment(){
-	cstring s1("continuous assignment");
-	cstring s2;
-	cstring s3;
-	s3 = s2 = s1;
-	s1.print();
-	s2.print();
-	s3.print();
+void continuous() {
+  cs s1("continuous");
+  cs s2, s3;
+  s3 = s2 = s1;
+  s1.print();
+  s2.print();
+  s3.print();
 }
-void assignmentToSelf(){
-	cstring s("assignment to self");
-	s = s;
-	s.print();
+void assignmentToSelf() {
+  cs s("assignment to self");
+  s = s;
+  s.print();
 }
-int main(){
-	assignment();
-	continuousAssignment();
-	assignmentToSelf();
-	return 0;
+int main() {
+  assignment();
+  continuous();
+  assignmentToSelf();
+  return 0;
 }
